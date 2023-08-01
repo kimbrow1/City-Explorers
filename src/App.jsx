c5de1d1aea6a7ce0885570fd255f757298d5e5e5
Brian Kimbrow <bkimbrow25@gmail.com>
2023-07-31 22:08:19 -0500

    map-update

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,28 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      memphisDisplay_Name: '', 
+      memphisDisplay_Name: '',
       lon: '',
-      lat: '', 
+      lat: '',
       memphisCityName: ''
     };
   }
 
   handleExplore = async (event) => {
     event.preventDefault();
-    let result = await axios.get(
-      `https://us1.locationiq.com/v1/search?key=${location_IQ_API_KEY}&q=${this.state.memphisCityName}&format=json`
-    );
-    console.log(result);
-    this.setState({
-      memphisDisplay_Name: result.data[0].display_name, 
-      lon: result.data[0].lon, 
-      lat: result.data[0].lat 
-    });
-    console.log(this.state);
+    try {
+      let result = await axios.get(
+        `https://us1.locationiq.com/v1/search?key=${location_IQ_API_KEY}&q=${this.state.memphisCityName}&format=json`
+      );
+
+      this.setState({
+        memphisDisplay_Name: result.data[0].display_name,
+        lon: result.data[0].lon,
+        lat: result.data[0].lat
+      });
+    } catch (error) {
+      console.error("Error fetching location data:", error);
+    }
   };
 
   handleChange = (event) => {
@@ -36,27 +39,30 @@ class App extends React.Component {
 
   render() {
     return (
-      <><form onSubmit={this.handleExplore}>
+      <>
+        <form onSubmit={this.handleExplore}>
           City Name:
-        
           <label>
             <input type="text" value={this.state.memphisCityName} onChange={this.handleChange} />
             <input type="submit" value="Submit" />
           </label>
-         
         </form>
 
         <h3>{this.state.memphisDisplay_Name}</h3>
         <p>Latitude: {this.state.lat}</p>
         <p>Longitude: {this.state.lon}</p>
-        
-        <img src={`https://us1.locationiq.com/v1/search?key=${location_IQ_API_KEY}&q=${this.state.memphisCityName}&format=json`} />
 
-        
+        {this.state.lat !== '' && this.state.lon !== '' && (
+          <img
+            src={`https://maps.locationiq.com/v3/staticmap?key=${location_IQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=12&size=400x400&format=png&maptype=map`}
+            alt="Map"
+          />
+        )}
       </>
     );
   }
 }
 
 export default App;
+
 
