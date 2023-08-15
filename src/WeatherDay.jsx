612e2e3697244892d99d8e0090f36c5197c5b13b
Brian Kimbrow <bkimbrow25@gmail.com>
2023-08-15 13:37:42 -0500

    update

--- src/WeatherDay.jsx
+++ src/WeatherDay.jsx
@@ -1,22 +1,3 @@
-// import React, { Component } from 'react'
-// import { Card } from 'react-bootstrap'
-
-// export default class WeatherDay extends Component {
-//   render() {
-//     const { title } = this.props.weatherData;
-//     return (
-//         <Card>
-//         <Card.Body>
-//           <Card.Text>{this.props.weatherData.overview}</Card.Text>
-//           <Card.Title>{this.props.weatherData.title}</Card.Title>
-//           <Card.Img src={this.props.weatherData.img} alt={"Poster for  " + title}
-//           />
-//         </Card.Body>
-//       </Card>
-//     )
-//   }
-// }
-
 import React, { Component } from 'react'
 import { Card } from 'react-bootstrap'
 
